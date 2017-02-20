613541829253bde566f330512ed55105dd0c5183
Benedikt Spranger <b.spranger@linutronix.de>
2017-02-20 19:14:39 +0100

    Add RT-Tests error parser
    Add a parser for RT-Tests output. The parser recognize WARN(ING) and FATAL
    output. The Output is categorized in priority normal for WARN and high
    for FATAL messages.
    
    For now only cyclictest is tested well. The other tests like cyclicdeadline,
    pip_stress, pi_stress, etc. may behave unexpected.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Reviewed-by: Holger Dengler <dengler@linutronix.de>

---
+++ src/main/java/hudson/plugins/warnings/parser/RTTestsParser.java
@@ -0,0 +1,43 @@
+package hudson.plugins.warnings.parser;
+
+import java.util.regex.Matcher;
+
+import hudson.Extension;
+
+import hudson.plugins.analysis.util.model.Priority;
+
+/**
+ * A parser for RTTests Error Messages.
+ *
+ * @author Benedikt Spranger
+ */
+@Extension
+public class RTTestsParser extends RegexpLineParser {
+	private static final long serialVersionUID = 2993328034978892249L;
+
+	/** Pattern of perforce compiler warnings. */
+	private static final String CYCLICTEST_WARNING_PATTERN =
+		"^(WARN(ING)?|FATAL):(.*)$";
+
+	/**
+     * Creates a new instance of {@link RTTestsParser}.
+     */
+	public RTTestsParser() {
+		super(Messages._Warnings_RTTests_ParserName(),
+			  Messages._Warnings_RTTests_LinkName(),
+			  Messages._Warnings_RTTests_TrendName(),
+			  CYCLICTEST_WARNING_PATTERN, true);
+	}
+
+	@Override
+	protected Warning createWarning(final Matcher matcher) {
+		String category = matcher.group(1).trim();
+		String message = matcher.group(3).trim();
+		Priority priority = Priority.NORMAL;
+
+		if (category.equals("FATAL"))
+			priority = Priority.HIGH;
+
+		return createWarning("Nil", 0, category, message, priority);
+	}
+}
