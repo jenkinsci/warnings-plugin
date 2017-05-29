cc93ce47ab3cb2a7971f033f5c84a5c18dde9e97
Benedikt Spranger <b.spranger@linutronix.de>
2017-05-29 14:33:56 +0200

    Codingstyle fixes
    Due to misconfigured indentation settings the code was misaligned.
    Perform a TAB equals 8 spaces rework. While at it, change some
    copy'n'paste relicts in comments. No functional change at all.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>

--- src/main/java/hudson/plugins/warnings/parser/RTTestsParser.java
+++ src/main/java/hudson/plugins/warnings/parser/RTTestsParser.java
@@ -13,31 +13,31 @@ import hudson.plugins.analysis.util.model.Priority;
  */
 @Extension
 public class RTTestsParser extends RegexpLineParser {
-	private static final long serialVersionUID = 2993328034978892249L;
+    private static final long serialVersionUID = 2993328034978892249L;
 
-	/** Pattern of perforce compiler warnings. */
-	private static final String CYCLICTEST_WARNING_PATTERN =
-		"^(WARN(ING)?|FATAL):(.*)$";
+    /** Pattern of RTTests warnings. */
+    private static final String CYCLICTEST_WARNING_PATTERN =
+        "^(WARN(ING)?|FATAL):(.*)$";
 
-	/**
+    /**
      * Creates a new instance of {@link RTTestsParser}.
      */
-	public RTTestsParser() {
-		super(Messages._Warnings_RTTests_ParserName(),
-			  Messages._Warnings_RTTests_LinkName(),
-			  Messages._Warnings_RTTests_TrendName(),
-			  CYCLICTEST_WARNING_PATTERN, true);
-	}
+    public RTTestsParser() {
+        super(Messages._Warnings_RTTests_ParserName(),
+              Messages._Warnings_RTTests_LinkName(),
+              Messages._Warnings_RTTests_TrendName(),
+              CYCLICTEST_WARNING_PATTERN, true);
+    }
 
-	@Override
-	protected Warning createWarning(final Matcher matcher) {
-		String category = matcher.group(1).trim();
-		String message = matcher.group(3).trim();
-		Priority priority = Priority.NORMAL;
+    @Override
+    protected Warning createWarning(final Matcher matcher) {
+        String category = matcher.group(1).trim();
+        String message = matcher.group(3).trim();
+        Priority priority = Priority.NORMAL;
 
-		if (category.equals("FATAL"))
-			priority = Priority.HIGH;
+        if (category.equals("FATAL"))
+            priority = Priority.HIGH;
 
-		return createWarning("Nil", 0, category, message, priority);
-	}
+        return createWarning("Nil", 0, category, message, priority);
+    }
 }
--- src/test/java/hudson/plugins/warnings/parser/RTTestsParserTest.java
+++ src/test/java/hudson/plugins/warnings/parser/RTTestsParserTest.java
@@ -15,53 +15,55 @@ import hudson.plugins.analysis.util.model.Priority;
  * Tests the class {@link RTTestsParser}.
  */
 public class RTTestsParserTest extends ParserTester {
-	private static final String TYPE = new RTTestsParser().getGroup();
-	private static final String FILENAME = "cyclictest.log";
+    private static final String TYPE = new RTTestsParser().getGroup();
+    private static final String FILENAME = "cyclictest.log";
 
-	/**
+    /**
      * Parses a file with three RTTests warnings.
+     *
+     * @author Benedikt Spranger
      *
      * @throws IOException
      *      if the file could not be read
      */
-	@Test
-	public void testWarningsParser() throws IOException {
-		Collection<FileAnnotation> warnings = new RTTestsParser().parse(openFile());
+    @Test
+    public void testWarningsParser() throws IOException {
+        Collection<FileAnnotation> warnings = new RTTestsParser().parse(openFile());
 
-		assertEquals("Wrong number of warnings detected.", 4, warnings.size());
+        assertEquals("Wrong number of warnings detected.", 4, warnings.size());
 
-		Iterator<FileAnnotation> iterator = warnings.iterator();
-		FileAnnotation annotation = iterator.next();
-		checkWarning(annotation,
-					 0,
-					 "Running on unknown kernel version...YMMV",
-					 "Nil",
-					 TYPE, "WARN", Priority.NORMAL);
+        Iterator<FileAnnotation> iterator = warnings.iterator();
+        FileAnnotation annotation = iterator.next();
+        checkWarning(annotation,
+                     0,
+                     "Running on unknown kernel version...YMMV",
+                     "Nil",
+                     TYPE, "WARN", Priority.NORMAL);
 
-		annotation = iterator.next();
-		checkWarning(annotation,
-					 0,
-					 "Running on unknown kernel version...YMMV",
-					 "Nil",
-					 TYPE, "WARNING", Priority.NORMAL);
+        annotation = iterator.next();
+        checkWarning(annotation,
+                     0,
+                     "Running on unknown kernel version...YMMV",
+                     "Nil",
+                     TYPE, "WARNING", Priority.NORMAL);
 
-		annotation = iterator.next();
-		checkWarning(annotation,
-					 0,
-					 "numa and smp options are mutually exclusive",
-					 "Nil",
-					 TYPE, "FATAL", Priority.HIGH);
+        annotation = iterator.next();
+        checkWarning(annotation,
+                     0,
+                     "numa and smp options are mutually exclusive",
+                     "Nil",
+                     TYPE, "FATAL", Priority.HIGH);
 
-		annotation = iterator.next();
-		checkWarning(annotation,
-					 0,
-					 "could not mount debugfs",
-					 "Nil",
-					 TYPE, "FATAL", Priority.HIGH);
-	}
+        annotation = iterator.next();
+        checkWarning(annotation,
+                     0,
+                     "could not mount debugfs",
+                     "Nil",
+                     TYPE, "FATAL", Priority.HIGH);
+    }
 
-	@Override
-	protected String getWarningsFile() {
-		return FILENAME;
-	}
+    @Override
+    protected String getWarningsFile() {
+        return FILENAME;
+    }
 }
