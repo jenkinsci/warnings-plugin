04d64c65a84ec7adcdb0dc790c612c1a50040f70
HOFSCHWEIGER Philipp <philipphofschweiger@gmail.com>
2018-06-14 15:15:54 +0200

    Changed MavenConsoleParser regex pattern to be more restrictive to avoid false positives in case of non-maven log messages containing [ERROR] or [WARNING].

--- src/main/java/hudson/plugins/warnings/parser/MavenConsoleParser.java
+++ src/main/java/hudson/plugins/warnings/parser/MavenConsoleParser.java
@@ -27,16 +27,31 @@ public class MavenConsoleParser extends RegexpLineParser {
 
     private static final long serialVersionUID = 1737791073711198075L;
 
-    private static final String PATTERN = "^.*\\[(WARNING|ERROR)\\]\\s*(.*)$";
+    /**
+     * Pattern for identifying warning or error maven logs.
+     * <pre>
+     * Pattern:
+     * (.*\s\s|)           -> Capture group 1 matches either empty string (e.g. [WARNING] some log) or some text followed by exactly two
+     *                        spaces (e.g. 22:07:27  [WARNING] some log)
+     * \[(WARNING|ERROR)\] -> Capture group 2 matches either [WARNING] or [ERROR]
+     * \s*                 -> matches zero or more spaces
+     * (.*)                -> Capture group 3 matches zero or more characters except line breaks, represents the actual error message
+     * </pre>
+     * <p>
+     * Typical maven logs:
+     * 1) 22:07:27  [WARNING] For this reason, future Maven versions might no longer support building such malformed projects.
+     * 2) [ERROR] The POM for org.codehaus.groovy.maven:gmaven-plugin:jar:1.1 is missing
+     */
+    private static final String PATTERN = "^(.*\\s\\s|)\\[(WARNING|ERROR)\\]\\s*(.*)$";
 
     /**
      * Creates a new instance of {@link MavenConsoleParser}.
      */
     public MavenConsoleParser() {
         super(Messages._Warnings_Maven_ParserName(),
-                Messages._Warnings_Maven_LinkName(),
-                Messages._Warnings_Maven_TrendName(),
-                PATTERN, true);
+              Messages._Warnings_Maven_LinkName(),
+              Messages._Warnings_Maven_TrendName(),
+              PATTERN, true);
     }
 
     @Override
@@ -48,7 +63,10 @@ public class MavenConsoleParser extends RegexpLineParser {
     protected Warning createWarning(final Matcher matcher) {
         Priority priority;
         String category;
-        if (ERROR.equals(matcher.group(1))) {
+        String errorOrWarningGroup = matcher.group(2);
+        String errorOrWarningMessage = matcher.group(3);
+
+        if (ERROR.equals(errorOrWarningGroup)) {
             priority = Priority.HIGH;
             category = "Error";
         }
@@ -56,7 +74,7 @@ public class MavenConsoleParser extends RegexpLineParser {
             priority = Priority.NORMAL;
             category = "Warning";
         }
-        return createWarning(CONSOLE, getCurrentLine(), category, matcher.group(2), priority);
+        return createWarning(CONSOLE, getCurrentLine(), category, errorOrWarningMessage, priority);
     }
 
     // FIXME: post processing is quite slow for large number of warnings, see JENKINS-25278
